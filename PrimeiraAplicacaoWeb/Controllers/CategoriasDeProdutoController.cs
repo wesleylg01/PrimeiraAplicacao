8e5078d31d4f0201ea17f735022935dc6ff543b7
Wesley Gonçalves <wesleylg01@gmail.com>
2021-05-02 17:24:10 -0300

    create a new controller and views

---
+++ PrimeiraAplicacaoWeb/Controllers/CategoriasDeProdutoController.cs
@@ -0,0 +1,28 @@
+﻿using PrimeiraAplicacaoWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PrimeiraAplicacaoWeb.Controllers
+{
+    public class CategoriasDeProdutoController : Controller
+    {
+        // GET: CategoriasDeProduto
+        public ActionResult Index(int? id)
+        {
+            ViewBag.ValorQS = id;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(CategotiaDeProduto categoria)
+        {
+            if (categoria == null)
+            {
+                return new HttpNotFoundResult();
+            }
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
---
+++ PrimeiraAplicacaoWeb/Models/CategotiaDeProduto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrimeiraAplicacaoWeb.Models
+{
+    public class CategotiaDeProduto
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+    }
+}
